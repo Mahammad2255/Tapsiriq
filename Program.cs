989e15f7539d3a70d6e99bce25bfff6ff206aba7
Mahammad2255 <[mahammadei@code.edu.az]>
2021-12-02 14:20:56 -0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Employee employee = new Employee();
+
+            employee.Maas("Mehemmed", "Ismayilov", "Developer", 50);
+            
+
+
+        }
+    }
+    class Employee
+    {
+
+        public string Name;
+        public string Surname;
+        public string Position;
+        public int DailySalary;
+        public int maas;
+        
+        public void Maas(string Name, string Surname, string Position, int DailySalary)
+        {
+            maas = DailySalary * 30;
+            Console.WriteLine($" {Name} {Surname} {Position} {maas}");
+        }
+    }
+}
+
